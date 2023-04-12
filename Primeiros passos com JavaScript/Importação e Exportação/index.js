a90647ccbb5a9e6e055b03e99ba16847a285315c
Biel-GT <gabrielsouzat2005@outlook.com>
2023-04-12 12:57:44 -0300

    Importação e Exportação

---
+++ Primeiros passos com JavaScript/Importação e Exportação/index.js
@@ -0,0 +1,15 @@
+
+const {gets, print} = require('./funcoes-auxiliares');
+
+/* 
+Descontruir objetos
+    const pessoa = {
+        nome: 'Gabriel'
+    };
+
+    const { nome } = pessoa;
+
+    Pegar atributo direto e como se fosse tirar ele do objeto, pra usar 
+*/
+
+print(gets());
